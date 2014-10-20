f298530142e4bb70fb59a75e355053851057bc9d
jonschlinkert <dev@sellside.com>
2014-10-20 11:32:41 -0400

    add tests

--- test.js
+++ test.js
@@ -13,11 +13,25 @@ var expand = require('./');
 
 describe('expand range', function () {
   it('should expand numerical ranges', function () {
-    expand('{1..3}').should.eql(['1', '2', '3']);
-    expand('{5..8}').should.eql(['5', '6', '7', '8']);
-    expand('a{1..3}b{1..4}d').should.eql(['a1b1d', 'a1b2d', 'a1b3d', 'a1b4d', 'a2b1d', 'a2b2d', 'a2b3d', 'a2b4d', 'a3b1d', 'a3b2d', 'a3b3d', 'a3b4d']);
-    // expand('a{1..3}b{1..3}d').should.eql(['a1b1d', 'a1b2d', 'a1b3d', 'a2b1d', 'a2b2d', 'a2b3d', 'a3b1d', 'a3b2d', 'a3b3d']);
-    // expand('a{1..5}b').should.eql(['a1b', 'a2b', 'a3b', 'a4b', 'a5b']);
-    // expand('a{00..05}b').should.eql(['a00b', 'a01b', 'a02b', 'a03b', 'a04b', 'a05b']);
+    expand('1..3').should.eql(['1', '2', '3']);
+    expand('5..8').should.eql(['5', '6', '7', '8']);
+  });
+
+  it('should honor padding', function () {
+    expand('00..05').should.eql(['00', '01', '02', '03', '04', '05']);
+    expand('01..03').should.eql(['01', '02', '03']);
+    expand('000..005').should.eql(['000', '001', '002', '003', '004', '005']);
+  });
+
+  it('should expand alphabetical ranges', function () {
+    expand('a..e').should.eql(['a', 'b', 'c', 'd', 'e']);
+    expand('A..E').should.eql(['A', 'B', 'C', 'D', 'E']);
+  });
+
+  it('should use a custom function for expansions.', function () {
+    var range = expand('a..e', function (str, i) {
+      return String.fromCharCode(str) + i;
+    });
+    range.should.eql(['a0', 'b1', 'c2', 'd3', 'e4']);
   });
 });
